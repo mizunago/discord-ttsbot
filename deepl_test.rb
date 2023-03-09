f3874163851fd4498283ad25f08033ef383f2e22
nagonago <mizunago@gmail.com>
2023-03-10 06:04:48 +0900

    DeepL のテストを追加

---
+++ deepl_test.rb
@@ -0,0 +1,34 @@
+require_relative 'deepl_trans'
+require 'active_support'
+require 'active_support/core_ext'
+require 'pp'
+
+def parse_env(envs)
+  params = {}.with_indifferent_access
+  envs.each do |env|
+    k, v = env.split('=')
+    params[k] = v
+  end
+  params
+end
+
+class Net::HTTP
+  alias :create :initialize
+
+  def initialize(*args)
+    create(*args)
+    self.set_debug_output $stderr
+    $stderr.sync = true
+  end
+end
+
+config = YAML.load(File.open('docker-compose.yml')).with_indifferent_access
+environment = parse_env(config[:services]["discord-ttsbot1"][:environment])
+
+DEEPL_AUTH_KEY = environment[:DEEPL_AUTH_KEY]
+DEEPL_PAID = !environment[:DEEPL_PAID].nil?
+puts "PAID STATUS: #{DEEPL_PAID}"
+
+deepl = Trans.new(DEEPL_AUTH_KEY, DEEPL_PAID)
+message = 'this is test. ignore me.'
+puts deepl.trans(message)
